dd136e40abcbd47c608c9f9db2454dd6b687fe39
Ankita <50921311+ankitatalwar17@users.noreply.github.com>
2021-09-09 23:10:49 +0530

    Interpretation of graph added

--- ex1.py
+++ ex1.py
@@ -1,3 +1,15 @@
+'''
+Graph Interpretation:
+
+ people live longer in countries with a higher GDP per capita.
+  No high income countries have really short life expectancy,
+  and no low income countries have very long life expectancy.
+  Still, there is a huge difference in life expectancy between countries
+  on the same income level. Most people live in middle income countries
+  where difference in lifespan is huge between countries;
+  depending on how income is distributed and how it is used.
+'''
+
 import matplotlib.pyplot as plt
 # Import numpy as np
 import numpy as np
