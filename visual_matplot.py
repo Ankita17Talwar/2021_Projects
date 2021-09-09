283cc18008fb661499fd07e59c091899c279fd16
Ankita <50921311+ankitatalwar17@users.noreply.github.com>
2021-09-09 22:08:55 +0530

    scatter plot

--- visual_matplot.py
+++ visual_matplot.py
@@ -3,5 +3,11 @@ import matplotlib.pyplot as plt
 year = [1950, 1970, 1990, 2010]
 pop = [2.159, 3.692, 5.263, 6.972]
 
+# Make a line plot: year on the x-axis, pop on the y-axis
 plt.plot(year, pop)
+# Display the plot with plt.show()
 plt.show()
+
+## Scatter Plot
+plt.scatter(year, pop)
+# plt.show()
